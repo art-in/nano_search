ef7f7dba816fa79b8143f5ce36d54c0e0756d7df
art-in <artin@artin.space>
2025-05-22 10:23:55 +0000

    refactoring: add comments for SearchEngine methods

--- src/model/engine.rs
+++ src/model/engine.rs
@@ -3,20 +3,27 @@ use anyhow::Result;
 use std::path::Path;
 
 pub trait SearchEngine {
+    /// Gets search engine name for debug logging purposes.
     fn get_name(&self) -> &'static str;
 
+    /// Initializes search engine and creates new index in target dir.
     fn create_index(index_dir: impl AsRef<Path>) -> Result<Self>
     where
         Self: Sized;
+
+    /// Initializes search engine and opens existing index from target dir.
     fn open_index(index_dir: impl AsRef<Path>) -> Result<Self>
     where
         Self: Sized;
 
+    /// Add documents into the index.
     fn index_docs(
         &mut self,
         // TODO: use iterator over doc reference
         docs: &mut dyn Iterator<Item = Doc>,
     ) -> Result<()>;
+
+    /// Searches for documents relevant for passed query in the index.
     fn search(&self, query: &str, limit: u64) -> Result<Vec<DocId>>;
 }
 
