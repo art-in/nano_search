42661a43c95c3c4c43d41a3425d9971607e21463
art-in <artin@artin.space>
2026-01-17 23:01:22 +0000

    refactoring: simplify TestDocsIterator

--- src/utils/test_docs_iterator.rs
+++ src/utils/test_docs_iterator.rs
@@ -1,16 +1,12 @@
+use std::vec::IntoIter;
+
 use crate::model::doc::Doc;
 
 pub struct TestDocsIterator {
-    docs: Vec<Doc>,
-    position: usize,
+    docs: IntoIter<Doc>,
 }
 
 impl TestDocsIterator {
-    #[must_use]
-    pub fn from_docs(docs: Vec<Doc>) -> Self {
-        Self { docs, position: 0 }
-    }
-
     #[must_use]
     pub fn from_enumerated_texts(texts: &Vec<(u64, &str)>) -> Self {
         Self {
@@ -20,20 +16,9 @@ impl TestDocsIterator {
                     id: *id,
                     text: text.to_string(),
                 })
-                .collect(),
-            position: 0,
+                .collect::<Vec<Doc>>()
+                .into_iter(),
         }
-    }
-
-    #[must_use]
-    pub fn from_texts(texts: &Vec<&str>) -> Self {
-        Self::from_enumerated_texts(
-            &texts
-                .iter()
-                .enumerate()
-                .map(|(id, text)| (id as u64, *text))
-                .collect(),
-        )
     }
 }
 
@@ -41,12 +26,6 @@ impl Iterator for TestDocsIterator {
     type Item = Doc;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.position < self.docs.len() {
-            let doc = self.docs[self.position].clone();
-            self.position += 1;
-            Some(doc)
-        } else {
-            None
-        }
+        self.docs.next()
     }
 }
